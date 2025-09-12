580fc4cf3d523806445794a6026189d21694bbbd
MMDS-LT02\mmdsd <gesang@merindo.co.id>
2025-09-12 15:19:43 +0700

    mengembalikan config.js ke dalam src/, fixing tombol

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // 🚀 Auto-generated file, jangan edit manual
-export const APP_CONFIG = {
-  BUILD_VERSION: '4.2.0 (bce429a)',
-};
+    export const APP_CONFIG = {
+      BUILD_VERSION: "4.2.0 (fc21e91)",
+    };
